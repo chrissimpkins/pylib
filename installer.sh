f83d8274fe3149c0950515bdefb6b07d7cb24962
Chris Simpkins <git.simpkins@gmail.com>
2016-03-12 21:48:04 -0500

    added an installer script

---
+++ installer.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Pulling the pylib archive..."
+echo " "
+curl -LO https://github.com/chrissimpkins/pylib/archive/master.tar.gz
+
+echo "Unpacking the archive..."
+echo " "
+tar -xzvf master.tar.gz
+mv pylib-master/* .
+
+echo "Cleaning up..."
+echo " "
+rm master.tar.gz
+rm installer.sh
+
+echo " "
+echo "Complete. The project files are available in the PROJECT directory."
